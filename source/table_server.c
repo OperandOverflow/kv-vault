c3e0aabd65e15354a4bab8dc9544ed49cd229c6b
OperandOverflow <wangxiting01917@gmail.com>
2023-12-04 03:22:00 +0000

    adaptacao a tabela replicada

--- source/table_server.c
+++ source/table_server.c
@@ -8,103 +8,44 @@
 
 #include "table.h"
 #include "table_skel.h"
-#include "zk_adaptor.h"
+#include "replica_table.h"
 #include "network_server.h"
+#include "replica_server_table.h"
 
-#include <errno.h>
 #include <stdio.h>
-#include <signal.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <zookeeper/zookeeper.h>
 
 struct table_t *table;
 int sockt;
 
-struct table_t *next_table;
-char* next_t_sock;
-
-// Variaveis globais para ZooKeeper
-const char *zsocket = "127.0.0.1:2181";
-zhandle_t *handler;
-char *zroot_path = "/table_servers";
-char* znode = NULL;
+s_rptable_t *repl_table;
 
 void inthandler() {
-    if (znode != NULL)
-        free(znode);
-    if (next_t_sock != NULL)
-        free(znode);
+    rptable_disconnect(repl_table);
     network_server_close(sockt);
     table_skel_destroy(table);
-    zookeeper_close(handler);
     exit(-1);
 }
 
-void connection_watcher(zhandle_t *zzh, int type, int state, const char *path, void* context) {
-	if (type == ZOO_SESSION_EVENT) {
-		if (state == ZOO_CONNECTED_STATE) {
-		} else { 
-            inthandler();
-		}
-	} 
+s_rptable_t *table_watcher() {
+    return repl_table;
 }
 
-void node_watcher(zhandle_t *zzh, int type, int state, const char *path, void* context) {
-    if (state != ZOO_CONNECTED_STATE)
-        inthandler();
-    
-    if (type != ZOO_CHILD_EVENT)
-        return;
-    
-    char* next = get_next_server(handler, zroot_path, znode, node_watcher);
-
-    // Se ainda nao ha nenhum servidor seguinte
-    if (next_t_sock == NULL) {
-        next_t_sock = next;
-        /**
-         * \todo
-         * Ligar ao servidor seguinte.
-        */
-        return;
-    } 
-
-    // Se o servidor seguinte ja foi abaixo e nao ha nenhum
-    // outro para o substituir
-    if (next == NULL) {
-        next_t_sock = NULL;
-        /**
-         * \todo 
-         * network_change_server(next_t_sock);
-        */
-        return;
-    }
-
-    // Se as duas sockets forem iguais
-    if (strcmp(next, next_t_sock) == 0) {
-        free(next);
-        return;
-    }
-
-    // Se o novo servidor seguinte e diferente do atual
-    free(next_t_sock);
-    next_t_sock = next;
-    
-    /**
-     * \todo 
-     * Ligar ao novo servidor.
-     * network_change_server(next_t_sock);
-    */
+void table_fhandler(int errcode) {
+    inthandler();
 }
 
 int main(int argc, char ** argv) {
-    if (argc != 3 && argc != 4) {
+    if (argc != 3) {
         printf("Wrong number of arguments!\n");
-        printf("Usage: <port> <table size> or <port> <table size> <zoo ip>:<zoo port>\n");
+        printf("Usage: <port> <table size>\n");
         return -1;
     }
  
@@ -139,57 +80,17 @@ int main(int argc, char ** argv) {
         return -1;
     }
 
-    zoo_set_debug_level(ZOO_LOG_LEVEL_ERROR);
-
-    // Ligar ao ZooKeeper
-    if ((handler = zookeeper_init(zsocket, connection_watcher, 2000000, 0, NULL, 0)) == NULL) {
-        perror("Error while connecting to ZooKeeper!\n");
-        network_server_close(sockt);
+    // Inicializar a tabela replicada
+    if ((repl_table = rptable_connect(sockt, table_watcher, table_fhandler)) == NULL) {
+        perror("Error while initializing replicated table!\n");
         table_skel_destroy(table);
-        return -1;
-    }
-
-    // Tentar criar o no raiz
-    int new_root_len = 1024;
-	char created_zroot_path[new_root_len];
-    while (ZNONODE == zoo_exists(handler, zroot_path, 0, NULL)) {
-        if (ZOK != zoo_create(handler, zroot_path, NULL, -1, 
-            &ZOO_OPEN_ACL_UNSAFE, 0, created_zroot_path, new_root_len)) {
-            printf("Error creating root!\n");
-            network_server_close(sockt);
-            table_skel_destroy(table);
-            zookeeper_close(handler);
-            return -1;
-        }
-    }
-
-    set_server_prefix("server");
-
-    // Criar um no efemero
-    if ((znode = register_server(handler, zroot_path, sockt)) == NULL) {
-        perror("Failed to register server on ZK!");
         network_server_close(sockt);
-        table_skel_destroy(table);
-        zookeeper_close(handler);
         return -1;
-    }
-
-    // Obter o socket do proximo servidor
-    next_t_sock = get_next_server(handler, zroot_path, znode, node_watcher);
-    if (next_t_sock != NULL) {
-        /**
-         * \todo
-         * Ligar ao servidor seguinte.
-        */
     }
 
     // Atender clientes (so nos dias uteis, das 9h ate as 16h)
     network_main_loop(sockt, table);
-    
-    free(znode);
-    free(next_t_sock);
     network_server_close(sockt);
     table_skel_destroy(table);
-    zookeeper_close(handler);
     return 0;
 }
