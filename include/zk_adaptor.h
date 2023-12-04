2eeb1bf5804dbec20b53725752e221f115c1b85d
OperandOverflow <wangxiting01917@gmail.com>
2023-12-04 03:16:08 +0000

    adicao e completacao funcoes

--- include/zk_adaptor.h
+++ include/zk_adaptor.h
@@ -1,20 +1,49 @@
+/**
+ * SD-07
+ * 
+ * Xiting Wang      <58183>
+ * Goncalo Pinto    <58178>
+ * Guilherme Wind   <58640>
+*/
+
+/**
+ * Módulo que adaptam as funcionalidades fornecidas pela
+ * biblioteca de ZooKeeper às operações necessárias no 
+ * projeto.
+*/
+
 #ifndef _ZK_ADAPTOR_H
 #define _ZK_ADAPTOR_H
 
 #include <zookeeper/zookeeper.h>
 
+/* ZooKeeper Znode Data Length (1MB, the max supported) */
+#define ZDATALEN 1024 * 1024
+typedef struct String_vector zoo_string; 
+
 // =========================================================
 //                    Operacoes gerais
 // =========================================================
-int set_node_watcher(zhandle_t* handler, char* node, watcher_fn watcher);
 
-int exists(zhandle_t* handler, char* node, watcher_fn watcher);
+/**
+ * Colocar uma funcao para observer alteracoes da estrutura
+ * de um no.
+ * \param handler
+ *      ZooKeeper handler.
+ * \param node
+ *      Caminho completo para o no.
+ * \param watcher
+ *      Funcao que e chamada quando detetar alguma alteracao.
+ * \return
+ *      0 (OK), 1 se o no nao existe e -1 em caso de erro.
+*/
+int set_node_watcher(zhandle_t* handler, char* node, watcher_fn watcher);
 
 // =========================================================
 //                  Operacoes do servidor
 // =========================================================
 /**
- * Define o prefixo do nome de servidor para registar servidor.
+ * Define o prefixo do no usado para registar o servidor.
  * \param nameprefix
  *      Apontador para o prefixo.
  * \return 
@@ -25,16 +54,28 @@ int set_server_prefix(char* nameprefix);
 /**
  * Retorna o prefixo do nome de servidor.
  * \return
- *      Apontador para a string que contem o prefixo, ou NULL
- *      caso nenhum prefixo ainda tenha sido definido ou ocorreu 
- *      um erro.
+ *      Apontador para a string que contem o prefixo, ou 
+ *      NULL caso nenhum prefixo ainda tenha sido definido  
+ *      ou ocorreu um erro.
 */
 char* get_server_prefix();
 
 /**
- * Regista o servidor atual no ZooKeeper, criando um no efemero
- * com o numero de sequencia e o prefixo, coloca o socket do 
- * servidor no no criado.
+ * Cria o no raiz.
+ * \param handler
+ *      ZooKeeper handler.
+ * \param path
+ *      Caminho completo para o no que pretende criar,
+ *      inclui o nome do proprio no.
+ * \return 
+ *      0 (OK), 1 se o no ja existe ou -1 em caso de erro.
+*/
+int create_root(zhandle_t *handler, char *path);
+
+/**
+ * Regista o servidor atual no ZooKeeper, criando um no 
+ * efemero com o numero de sequencia e o prefixo, coloca  
+ * o socket do servidor no no criado.
  * \param handler
  *      ZooKeeper handler.
  * \param path
@@ -47,8 +88,8 @@ char* get_server_prefix();
 char* register_server(zhandle_t* handler, char* path, int socket);
 
 /**
- * Retorna o descritor do proximo servidor em string no formato
- * <ip>:<porto>.
+ * Retorna o descritor do proximo servidor em string 
+ * no formato <ip>:<porto>.
  * \param handler
  *      ZooKeeper handler.
  * \param rootpath
@@ -58,8 +99,8 @@ char* register_server(zhandle_t* handler, char* path, int socket);
  * \param watcher
  *      Funcao watcher para observer o diretorio que tem os nos.
  * \return 
- *      Descritor do socket do servidor seguinte ou NULL se nao 
- *      houver nenhum servidor seguinte ou ocorreu erro.
+ *      Descritor do socket do servidor seguinte ou NULL se 
+ *      nao houver nenhum servidor seguinte ou ocorreu erro.
 */
 char* get_next_server(zhandle_t* handler, char* rootpath, char* node, watcher_fn watcher);
 
@@ -68,21 +109,41 @@ char* get_next_server(zhandle_t* handler, char* rootpath, char* node, watcher_fn
 // =========================================================
 
 /**
- * Retorna o socket do servidor mais antigo em string no
- * formato <ip>:<porto>.
+ * Retorna o socket do servidor com o menor identificador 
+ * (mais antigo) em string no formato <ip>:<porto>.
  * \param handler
  *      ZooKeeper handler.
+ * \param path
+ *      Caminho ao no que contem os nos dos servidores.
+ * \param watcher
+ *      Funcao que e invocada quando houver alguma alteracao no no.
+ * \return
+ *      Descritor do socket do servidor na cabeca, ou NULL 
+ *      se nao encontrar nenhum servidor ou ocorreu um erro.
 */
 char* get_head_server(zhandle_t* handler, char* path, watcher_fn watcher);
 
+/**
+ * Retorna o socket do servidor com o maior identificador 
+ * (mais novo) em string no formato <ip>:<porto>.
+ * \param handler
+ *      ZooKeeper handler.
+ * \param path
+ *      Caminho ao no que contem os nos dos servidores.
+ * \param watcher
+ *      Funcao que e invocada quando houver alguma alteracao no no.
+ * \return
+ *      Descritor do socket do servidor na cauda, ou NULL se nao
+ *      houver nenhum servidor ou ocorreu um erro.
+*/
 char* get_tail_server(zhandle_t* handler, char* path, watcher_fn watcher);
 
 
 
 
 // ================== Funcoes privadas ====================
-static void search_node(zhandle_t* handler, char* path, watcher_fn watcher);
+// static void search_node(zhandle_t* handler, char* path, watcher_fn watcher);
 
-static void search_node_dispatcher(char const * caller_name, zhandle_t* handler, char* path, watcher_fn watcher);
+// static void search_node_dispatcher(char const * caller_name, zhandle_t* handler, char* path, watcher_fn watcher);
 
 #endif
--- source/zk_adaptor.c
+++ source/zk_adaptor.c
@@ -1,3 +1,11 @@
+/**
+ * SD-07
+ * 
+ * Xiting Wang      <58183>
+ * Goncalo Pinto    <58178>
+ * Guilherme Wind   <58640>
+*/
+
 #include "zk_adaptor.h"
 
 #include <stdio.h>
@@ -11,11 +19,25 @@
 
 char *server_name_prefix = NULL;
 
-/* ZooKeeper Znode Data Length (1MB, the max supported) */
-#define ZDATALEN 1024 * 1024
-typedef struct String_vector zoo_string; 
+int set_node_watcher(zhandle_t* handler, char* node, watcher_fn watcher) {
+    if (handler == NULL || node == NULL || watcher == NULL)
+        return -1;
+    
+    int result;
+    if (ZOK == (result = zoo_wexists(handler, node, watcher, NULL, NULL)))
+        return 0;
+    if (result == ZNONODE)
+        return 1;
+    return -1;
+}
 
 int set_server_prefix(char* nameprefix) {
+    if (server_name_prefix != NULL)
+        free(server_name_prefix);
+    if (nameprefix == NULL) {
+        server_name_prefix = NULL;
+        return 0;
+    }
     server_name_prefix = strdup(nameprefix);
     if (server_name_prefix == NULL)
         return -1;
@@ -26,6 +48,32 @@ char* get_server_prefix() {
     if (server_name_prefix == NULL)
         return NULL;
     return strdup(server_name_prefix);
+}
+
+int create_root(zhandle_t *handler, char *path) {
+    if (handler == NULL || path == NULL)
+        return -1;
+
+    if (ZOK == zoo_exists(handler, path, 0, NULL))
+        return 1;
+    
+    while (ZNONODE == zoo_exists(handler, path, 0, NULL)) {
+        int res = zoo_create(handler, path, NULL, -1, 
+                    &ZOO_OPEN_ACL_UNSAFE, 0, NULL, 0);
+        switch (res) {
+        case ZNONODE:
+            return -1;
+            break;
+        case ZBADARGUMENTS:
+            return -1;
+            break;
+        
+        default:
+            continue;
+            break;
+        }
+    }
+    return 0;
 }
 
 char* register_server(zhandle_t* handler, char* path, int socket) {
@@ -80,6 +128,11 @@ char* register_server(zhandle_t* handler, char* path, int socket) {
     return node_name;
 }
 
+// Procura o no imediatamente com o identificador maior
+/**
+ * \todo
+ * Procurar o no com identificador seguinte em vez de 
+*/
 char* get_next_server(zhandle_t* handler, char* rootpath, char* node, watcher_fn watcher) {
     if (handler == NULL || rootpath == NULL || node == NULL)
         return NULL;
@@ -135,6 +188,7 @@ char* get_next_server(zhandle_t* handler, char* rootpath, char* node, watcher_fn
     char* zdata_buf = malloc(ZDATALEN * sizeof(char));
     int zdata_len = ZDATALEN * sizeof(char);
     if (ZOK != zoo_wget(handler, next_node, NULL, NULL, zdata_buf, &zdata_len, NULL)){
+        free(zdata_buf);
         free(next_node);
         free(node_list);
         return NULL;
@@ -184,7 +238,7 @@ char* get_head_server(zhandle_t* handler, char* path, watcher_fn watcher) {
             continue;
         }
 
-        // Se o novo e igual ou menor do que o atual
+        // Se o novo e menor do que o atual
         // strcmp("002", "001") = 1
         if (strcmp(node_fullpath, head_node) < 0) {
             // Liberta o atual e substitui pelo novo
@@ -216,12 +270,84 @@ char* get_head_server(zhandle_t* handler, char* path, watcher_fn watcher) {
     return zdata_buf;
 }
 
-#define search_node(zhandle_t, char, watcher_fn) (search_node_dispatcher)(__func__, zhandle_t, char, watcher_fn)
+char* get_tail_server(zhandle_t* handler, char* path, watcher_fn watcher) {
+    if (handler == NULL || path == NULL)
+        return NULL;
+    
+    // Alocar memoria para o buffer
+    zoo_string* node_list = malloc(sizeof(zoo_string));
+    if (node_list == NULL)
+        return NULL;
+    
+    // Obter a lista de nos
+    if (ZOK != zoo_wget_children(handler, path, watcher, NULL, node_list)) {
+        free(node_list);
+        return NULL;
+    }
 
-static void search_node_dispatcher(char const * caller_name, zhandle_t* handler, char* path, watcher_fn watcher) {
+    char* tail_node = NULL;
+    // Iterar pela lista de nos, procurar o no com o menor identificador
+    for (size_t i = 0; i < node_list->count; i++) {
+        // Alocar espaco para o caminho completo do no
+        char* node_fullpath = malloc(strlen(path) + strlen(node_list->data[i]) + 2);
+        if (node_fullpath == NULL) { 
+            free(node_list);
+            return NULL;
+        }
+        strcpy(node_fullpath, path);
 
+        // Verificar se tem / no fim do diretorio
+        if (node_fullpath[strlen(path) - 1] != '/')
+            strcat(node_fullpath, "/");
+        
+        // Concatenar o nome do no ao diretorio raiz
+        strcat(node_fullpath, node_list->data[i]);
+
+        // Se nao ainda ha nenhum no com o menor descritor
+        if (tail_node == NULL) {
+            tail_node = node_fullpath;
+            continue;
+        }
+
+        // Se o novo e maior do que o atual
+        // strcmp("002", "001") = 1
+        // dst - src
+        if (strcmp(node_fullpath, tail_node) > 0) {
+            // Liberta o atual e substitui pelo novo
+            free(tail_node);
+            tail_node = node_fullpath;
+            continue;
+        }
+
+        free(node_fullpath);
+    }
+    
+    // Se nada encontrou
+    if (tail_node == NULL) {
+        free(node_list);
+        return NULL;
+    }
+
+    // Alocar buffer para obter o conteudo do no
+    char* zdata_buf = malloc(ZDATALEN * sizeof(char));
+    int zdata_len = ZDATALEN * sizeof(char);
+    if (ZOK != zoo_wget(handler, tail_node, NULL, NULL, zdata_buf, &zdata_len, NULL)){
+        free(tail_node);
+        free(node_list);
+        return NULL;
+    }
+
+    free(tail_node);
+    free(node_list);
+    return zdata_buf;
 }
 
-static void (search_node)(zhandle_t* handler, char* path, watcher_fn watcher) {
+// #define search_node(zhandle_t, char, watcher_fn) (search_node_dispatcher)(__func__, zhandle_t, char, watcher_fn)
 
-}
+// static void search_node_dispatcher(char const * caller_name, zhandle_t* handler, char* path, watcher_fn watcher) {
+
+// }
+
+// static void (search_node)(zhandle_t* handler, char* path, watcher_fn watcher) {
+
+// }
